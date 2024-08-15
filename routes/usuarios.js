c557ac97df22e9b8457b1dee712190383068b382
gnjaramillo <gnjaramillo1986@gmail.com>
2024-08-15 12:17:23 -0500

    funcionalidad crear solicitud generando consecutivo del caso

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { getUsuarios, postUsuarios, updateUsuarios, deleteUsuarios, getUsuariosId } = require("../controllers/usuarios");
+const { getUsuarios, updateUsuarios, deleteUsuarios, getUsuariosId } = require("../controllers/usuarios");
 const uploadMiddleware = require("../utils/handleStorage");
 const { validatorPostUsuarios, validatorUpdateUsuarios, validatorPostUsuariosId } = require("../validators/usuarios");
 
 // Rutas para la manipulación de usuarios
 router.get("/", getUsuarios);
 router.get("/:id", validatorPostUsuariosId, getUsuariosId);
-router.post("/", uploadMiddleware.single('foto'), validatorPostUsuarios, postUsuarios);
 router.put("/:id", uploadMiddleware.single('foto'), validatorUpdateUsuarios, updateUsuarios);
 router.delete("/:id", deleteUsuarios);
 
