6afde0b6cbf4ce0ea0282a7047325d9a0a439969
gnjaramillo <gnjaramillo1986@gmail.com>
2024-08-22 13:47:47 -0500

    se añade funcionalidad recuperar contraseña, middleware para rol y session

--- routes/ambienteFormacion.js
+++ routes/ambienteFormacion.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
-// const authMiddleware = require('../middleware/session')
+const authMiddleware = require('../middleware/session')
 const checkRol = require('../middleware/rol')
 const {getAmbiente, getAmbienteId, postAmbiente, updateAmbiente, deleteAmbiente } = require("../controllers/ambienteFormacion")
 
 router.get("/", getAmbiente);
 router.get("/:id",    getAmbienteId);
-router.post("/", checkRol(['lider']), postAmbiente);
+router.post("/", authMiddleware, checkRol(['lider']), postAmbiente);
 router.put("/:id",  updateAmbiente);
 router.delete("/:id",  deleteAmbiente);
 
