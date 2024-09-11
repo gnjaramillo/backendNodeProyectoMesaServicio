34e475bcbbb42f625607efc02430435574ec8494
gnjaramillo <gnjaramillo1986@gmail.com>
2024-09-11 10:12:15 -0500

    se corrige error con  cors

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ const morgan = require("morgan");
 // Configuración de Express en la app
 app.use(cors({
   // origin: [process.env.FRONTEND_URL],
-  origin: '*',
+  origin: 'http://localhost:5173',
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
